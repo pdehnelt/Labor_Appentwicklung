4da403b770622845cde90d199ce67713f0d01a12
Phuong Dehnelt <pdehnelt@stud.hs-heilbronn.de>
2024-10-24 08:37:37 +0200

    abgabe 2

--- demo/src/main/java/org/example/aufgabe/CloudConsumptionApp.java
+++ demo/src/main/java/org/example/aufgabe/CloudConsumptionApp.java
@@ -1,17 +1,14 @@
 package org.example.aufgabe;
 
-
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.core.type.TypeReference;
 
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.io.OutputStream;
 import java.net.HttpURLConnection;
 import java.net.URL;
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 
 public class CloudConsumptionApp {
 
@@ -35,9 +32,8 @@ public class CloudConsumptionApp {
 
             if (conn.getResponseCode() == 200) {
                 BufferedReader in = new BufferedReader(new InputStreamReader(conn.getInputStream()));
-                String inputLine;
                 StringBuilder response = new StringBuilder();
-
+                String inputLine;
                 while ((inputLine = in.readLine()) != null) {
                     response.append(inputLine);
                 }
